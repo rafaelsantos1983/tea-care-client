d634d7a86d6a1dfe487042a2cd20a3f034ee191c
AynaAraujo <ayna.araujo@upe.br>
2024-06-20 21:19:48 -0300

    5-Modificações nos links

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login.jsx";
-<<<<<<< HEAD
 import Dashboard_Pais from "./Dashboard_Pais.jsx"
 import NewSenha from "./NewSenha";
-=======
-import Dashboard_Pais from "./Dashboard_Pais.jsx";
->>>>>>> 3c85070ac001e1e046e8284f556b568582df3521
 
 function App() {
   
@@ -14,9 +10,6 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
-        </Routes>
-        <Routes>
-          <Route exact path="/pais" element={<Dashboard_Pais />} />
         </Routes>
         <Routes>
           <Route path="/Dashboard_Pais" element={<Dashboard_Pais />} />
