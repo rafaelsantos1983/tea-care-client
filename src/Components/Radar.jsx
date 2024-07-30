9dada68aac411c3f160f4caf4756b1978de47690
Richard Jeremias <ricmartinsrocha@gmail.com>
2024-07-30 08:36:34 -0300

    Renderizando informacoes e legendas corretamente

--- src/Components/Radar.jsx
+++ src/Components/Radar.jsx
@@ -68,7 +68,9 @@ class Radar extends React.Component {
           min: 0,
           max: 5,
           tickAmount: 6,
+          show: false, // Esconde as labels do eixo y no gráfico
           labels: {
+            show: false, // Assegura que as labels do eixo y não serão renderizadas
             formatter: function(val) {
               if (val >= 0 && val < 1) {
                 return 'Muito Ruim';
@@ -86,15 +88,50 @@ class Radar extends React.Component {
             }
           }
         },
+        legend: {
+          show: false // Esconde a legenda padrão
+        }
       },
     };
   }
 
   render() {
+    const categories = [
+      "Comunicação",
+      "Alimentação",
+      "Habilidades Sociais",
+      "Comportamento",
+      "Autonomia E Autoregulação",
+      "Habilidades Motoras E Práxicas",
+      "Habilidades Acadêmicas"
+    ];
+
+    const yLabels = [
+      { label: 'Muito Ruim', color: '#ff0000' },
+      { label: 'Ruim', color: '#ff8000' },
+      { label: 'Médio', color: '#ffff00' },
+      { label: 'Bom', color: '#80ff00' },
+      { label: 'Excelente', color: '#00ff00' }
+    ];
+
     return (
       <div>
         <div id="chart">
           <ReactApexChart options={this.state.options} series={this.state.series} type="radar" width={450} height={300}/>
+        </div>
+        <div id="yaxis-labels" style={{ textAlign: 'center', marginTop: '10px', display: 'flex', justifyContent: 'center' }}>
+          {yLabels.map((item, index) => (
+            <div key={index} style={{ display: 'inline-block', margin: '0 10px' }}>
+              <span style={{ backgroundColor: item.color, color: '#fff', padding: '5px 10px', borderRadius: '5px' }}>{item.label}</span>
+            </div>
+          ))}
+        </div>
+        <div id="categories" style={{ marginTop: '20px', textAlign: 'center', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+          {categories.map((category, index) => (
+            <div key={index} style={{ display: 'inline-block', margin: '0 10px' }}>
+              <span style={{ backgroundColor: '#043058', color: '#fff', padding: '5px 10px', borderRadius: '5px' }}>{category}</span>
+            </div>
+          ))}
         </div>
         <div id="html-dist"></div>
       </div>
